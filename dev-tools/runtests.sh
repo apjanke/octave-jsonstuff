745bec7c3e88c69487f1cc9bebfc2318eea2c795
Andrew Janke <floss@apjanke.net>
2019-01-22 19:32:45 -0500

    Travis: add running tests

---
+++ dev-tools/runtests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Test wrapper for Octave tests.
+#
+# This exists because Octave's runtests() does not return an error status, so
+# you can't detect within octave whether your tests have failed.
+#
+# This must be run from the root of the repo.
+
+set -e
+
+test_dir="$1"
+
+tempfile=$(mktemp /tmp/octave-jsonstuff-tests-XXXXXXXX)
+if [[ "$test_dir" == "" ]]; then
+  octave --path="$PWD/inst" --eval="runtests" &>$tempfile
+else
+  octave --path="$PWD/inst" --eval="addpath('$test_dir'); runtests $test_dir" &>$tempfile
+fi
+
+cat $tempfile
+
+if grep -i '!!!!! test failed' $tempfile &>/dev/null; then
+  echo Some tests FAILED!
+  exit 1
+else
+  echo All tests passed.
+  exit 0
+fi
--- doc-project/TODO.md
+++ doc-project/TODO.md
@@ -1,12 +1,24 @@
 JsonStuff TODO
 ==============
 
+## Code
+
 * Tests
 * jsondecode
+  * Everything
   * oct file jsoncpp wrapper
+  * Extension: field-filling for arrays of objects -> struct array instead of cell array of structs
 * [UBJSON](https://en.wikipedia.org/wiki/UBJSON) support
 * Other TODOs scattered around the code
 * Probably OOP-ify the code (keeping `jsonencode`/`jsondecode`) as front-end function wrappers
   * So I can support various encoding/decoding options
 * Maybe octfile-ify jsonencode, for speed
 * Maybe support Java objects?
+
+## Project
+
+* Get TravisBuddy working
+* Travis build
+  * run octave: pkg install
+  * run octave: package tests
+
