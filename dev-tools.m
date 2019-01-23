2c50b03e4104dec27afff914a207819de75d4454
janke <janke@ubix.localdomain>
2019-01-23 08:38:49 -0800

    Remove bogus dev-tools.m file.
    Don't know where that came from.
    [ci skip]

--- dev-tools.m
+++
@@ -1,6 +0,0 @@
-function out = jsondecode_test
-  str = '[[1, 2.3456, null], {"foo": 42, "bar": "a string", "baz": [1 2 3], "qux": 1.23]';
-  str = '[[1, 2.3456, null], {"foo": 42, "bar": "a string", "baz": [1 2 3], "qux": 1.23}]';
-  str = '[[1, 2.3456, null], {"foo": 42, "bar": "a string", "baz": [1, 2, 3], "qux": {"whatever": [1, 42, null]}}]';
-  out = jsondecode (str);
-endfunction
